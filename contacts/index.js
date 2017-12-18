690c97af3f6df5709e6b45500e28f54cf23fd6bd
Underwaterz <h.bam_1@hotmail.com>
2017-12-18 21:47:38 +0700

    Create contactlist.

---
+++ contacts/index.js
@@ -0,0 +1,10 @@
+
+var contactList = [
+  {"name": "Ned Stark", "email": "ned@winterfell.com", "phone": "123-456-7890", "url": "www.google.com", "notes": "Winter is coming."},
+  {"name": "Theon Greyjoy", "email": "tgreyjoy@winterfell.com", "phone": "123-456-7890", "url": "www.google.com", "notes": "Reluctant to pay iron price."},
+  {"name": "Samwell Tarly", "email": "starly@castleblack.com", "phone": "123-456-7890", "url": "www.google.com", "notes": "Loyal brother of the watch."},
+  {"name": "Jon Snow", "email": "jsnow@castleblack.com", "phone": "123-456-7890", "url": "www.google.com", "notes": "Knows nothing."},
+  {"name": "Arya Stark", "email": "waterdancer@winterfell.com", "phone": "123-456-7890", "url": "www.google.com", "notes": "Has a list of names."},
+  ];
+
+
